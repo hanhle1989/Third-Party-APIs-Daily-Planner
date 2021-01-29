3979002a7a62f8397b2a3a2c22b483dae4696168
Hanh Le <hanhdtle@gmail.com>
2021-01-28 23:35:57 -0800

    fixing the local storage problem

--- script.js
+++ script.js
@@ -2,46 +2,44 @@ var m = moment();
 var currentDate = m.format("MMM Do YYYY");
 var currentHour = moment().hours();
 
+var saveBtn = $(".saveBtn");
 
-$(document).ready(function () {
-    $("#currentDay").text("Today's Date: " + currentDate);
-
-
-    $(".saveBtn").on("click", function () {
-        var click = $(this).attr("hour-value");
-        var eventInput = $(click).val();
-        localStorage.setItem(click, eventInput);
-    });
-
-    var timeBlock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
-    for (i = 0; i < timeBlock.length; i++) {
+let timeBlock = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
+    for (let i = 0; i < timeBlock.length; i++) {
         var savedEvent = $(".saved-event")
         $(timeBlock[i]).val(localStorage.getItem(timeBlock[i]));
     };
 
-    function timeTable() {
-        for (i = 6; i < 18; i++) {
+    
+$(document).ready(function () {
 
-            var hour = '#' + i;
-            var scheduleHour = parseInt($(hour).attr("id"));
-            $(hour).removeClass();
-            if (
-                scheduleHour === currentHour) {
-                $(hour).attr("class", "present");
+    //retrieve and display current date//
+    $("#currentDay").text("Today's Date: " + currentDate);
+    
+        for (var i = 1; i <= 12; i++) {
+            var scheduleHour = $("#" + i + "Row").attr("hour-data");
+            var hour = "#" + i
+            var scheduleHour = parseInt(hour);
+            
+            if (scheduleHour === currentHour) {
+                $(hour).addClass("present");
             }
 
-            else if (
-                scheduleHour > currentHour) {
-                $(hour).attr("class", "future");
+            else if (scheduleHour > currentHour) {
+                $(hour).addClass("future");
             }
 
-            else if (
-                scheduleHour < currentHour) {
-                $(hour).attr("class", "past");
+            else if (scheduleHour < currentHour) {
+                $(hour).addClass("past");
             }
 
         };
     }
-    timeTable();
 
+);
+
+saveBtn.on("click", function () {
+    var click = $(this).attr("hour-value");
+    var eventInput = $(click).val();
+    localStorage.setItem(click, eventInput);
 });
